c1727f5994bb14d57d304f3653531224e1d0184e
soapsama7 <1692226484@qq.com>
2026-07-08 01:37:52 +0800

    [Round 10] DemoApplication.main() 显式关闭 headless 模式，解决剪贴板 HeadlessException

--- src/main/java/com/audaque/demo/DemoApplication.java
+++ src/main/java/com/audaque/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// 必须在 AWT 加载前关闭 headless 模式，否则剪贴板监听会抛 HeadlessException
+		System.setProperty("java.awt.headless", "false");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
